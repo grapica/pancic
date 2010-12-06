cd6a603eb42c33de2e51587646b3a190a3f84dde
Milan Jovanovic <grapa2009@yahoo.com>
2010-12-06 14:42:06 +0100

    fifth commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,10 @@ class ApplicationController < ActionController::Base
   include LoginSystem
   # Scrub sensitive parameters from your log
   # filter_parameter_logging :password
+  before_filter :instantiate_controller_and_action_names
+
+def instantiate_controller_and_action_names
+      @current_action = action_name
+      @current_controller = controller_name
+end 
 end
--- app/models/application.rb
+++ app/models/application.rb
@@ -1,9 +1,4 @@
 class Application < ActiveRecord::Base
-before_filter :instantiate_controller_and_action_names
 
-def instantiate_controller_and_action_names
-      @current_action = action_name
-      @current_controller = controller_name
-end 
 
 end
