a7ffbbcb2fbdf77c54a6670c58e5a964033b6200
Milan Jovanovic <grapa2009@yahoo.com>
2010-12-06 12:45:44 +0100

    fourth commit

--- config/deploy.rb
+++ config/deploy.rb
@@ -35,4 +35,16 @@ namespace :deploy do
   task :restart, :roles => :app do
     run "touch #{current_release}/tmp/restart.txt"
   end
+  
+  task :cold do       # Overriding the default deploy:cold
+    update
+    load_schema       # My own step, replacing migrations.
+    start
+  end
+
+  task :load_schema, :roles => :app do
+    run "cd #{current_path}; rake db:schema:load"
+  end
+
+  
 end
